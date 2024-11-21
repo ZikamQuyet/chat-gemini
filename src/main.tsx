2b47785a36688682d33df71e361c158bc67f21e1
nmq <nmq120898@gmail.com>
2024-11-21 23:00:25 +0700

    fix deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -32,9 +31,7 @@ const router = createBrowserRouter([
   },
 ]);
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  // </StrictMode>
 );
--- src/store/chatSlice.ts/index.ts
+++ src/store/chatSlice.ts/index.ts
@@ -1,4 +1,3 @@
-import { text } from "@fortawesome/fontawesome-svg-core";
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 import { marked } from "marked";
